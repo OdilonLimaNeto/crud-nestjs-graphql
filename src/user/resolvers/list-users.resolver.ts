1cf638e745d8a4f86741c8edea3e9e48a6f74904
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-07-23 23:55:14 -0500

    ♻️ refactoring the user list

--- src/user/resolvers/list-users.resolver.ts
+++ src/user/resolvers/list-users.resolver.ts
@@ -1,17 +1,13 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import User from '../entities/users.entity';
-import ListFilterUserInput from '../inputs/list-filter.input';
 import ListUsersService from '../services/list-users.service';
-//TODO: corrigir tipos de filtragem
 @Resolver(() => User)
 class ListUsersResolver {
   constructor(private usersService: ListUsersService) {}
 
   @Query(() => [User])
-  async list(
-    @Args('input', { nullable: true }) input?: ListFilterUserInput,
-  ): Promise<User[]> {
-    const listAllUsers = await this.usersService.list(input as string);
+  async list(): Promise<User[]> {
+    const listAllUsers = await this.usersService.list();
     return listAllUsers;
   }
 }
