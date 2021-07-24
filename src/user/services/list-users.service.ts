c611816f3d82cc8caa7700a7dd3f038fae5c0f5c
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-07-23 23:55:57 -0500

    ♻️ refactoring the user list service

--- src/user/services/list-users.service.ts
+++ src/user/services/list-users.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { FindOperator, Like } from 'typeorm';
 import User from '../entities/users.entity';
 import UsersRepository from '../repositories/users-repository';
 
@@ -7,18 +6,9 @@ import UsersRepository from '../repositories/users-repository';
 class ListUsersService {
   constructor(private usersRepository: UsersRepository) {}
 
-  async list(id?: string, name?: string, email?: string): Promise<User[]> {
-    const filters: {
-      id?: FindOperator<string>;
-      name?: FindOperator<string>;
-      email?: FindOperator<string>;
-    } = {};
-
-    if (id) filters.id = Like(`${id}%`);
-    if (name) filters.name = Like(`${name}%`);
-    if (email) filters.email = Like(`${email}%`);
-
-    return this.usersRepository.find(filters);
+  async list(): Promise<User[]> {
+    const allUsers = await this.usersRepository.find();
+    return allUsers;
   }
 }
 
