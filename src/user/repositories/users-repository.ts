7fc564848077268e6c209f2972fff7ac6802c980
OdilonLimaNeto <odilongeronimoo.com>
2021-08-01 23:57:52 -0500

    ♻️ adding method isolation

--- src/user/repositories/users-repository.ts
+++ src/user/repositories/users-repository.ts
@@ -1,7 +1,21 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, Repository } from 'typeorm';
 import User from '../entities/users.entity';
+import CreateUserInput from '../inputs/create-user.input';
 
 @EntityRepository(User)
-class UsersRepository extends Repository<User> {}
+class UsersRepository extends Repository<User> {
+  private repository: Repository<User>;
+
+  constructor() {
+    super();
+    this.repository = getRepository(User);
+  }
+
+  async createUser(input: CreateUserInput) {
+    const userToCreate = this.repository.create(input);
+    const createdUser = await this.repository.save(userToCreate);
+    return createdUser;
+  }
+}
 
 export default UsersRepository;
